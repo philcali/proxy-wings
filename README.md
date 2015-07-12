0b6a87a23fc9f5e29f9e689be80a6110d837e259
Philip Cali <philip.cali@gmail.com>
2015-07-11 22:29:38 -0400

    Initialize the carwings proxy.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Carwings Proxy Service
+
+This REST service solely exists as a proxy between
+the Carwings SOAP service and pebble-carwings.
---
+++ project/Build.scala
@@ -0,0 +1,63 @@
+import sbt._
+import Keys._
+
+object Build extends sbt.Build {
+  override lazy val settings = super.settings ++ Seq(
+    organization := "philcali.github.com",
+    version := "1.0.0",
+    scalaVersion := "2.11.5"
+  )
+
+  lazy val root = Project(
+    "carwings",
+    file(".")
+  ) aggregate (data, client, dynamo, api, server)
+
+  lazy val data = Project(
+    "carwings-data",
+    file("data")
+  )
+
+  lazy val argonaut = Project(
+    "carwings-argonaut",
+    file("argonaut"),
+    settings = Seq(
+      libraryDependencies += "io.argonaut" %% "argonaut" % "6.0.4"
+    )
+  ) dependsOn data
+
+  lazy val dynamo = Project(
+    "carwings-dynamo",
+    file("dynamo"),
+    settings = Seq(
+      libraryDependencies += "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.10.4"
+    )
+  ) dependsOn data
+
+  lazy val client = Project(
+    "carwings-client",
+    file("client"),
+    settings = Seq(
+      libraryDependencies ++= Seq(
+        "org.scala-lang.modules" %% "scala-xml" % "1.0.4",
+        "net.databinder.dispatch" %% "dispatch-core" % "0.11.2"
+      )
+    )
+  ) dependsOn data
+
+  lazy val api = Project(
+    "carwings-api",
+    file("api"),
+    settings = Seq(
+      libraryDependencies += "net.databinder" %% "unfiltered-directives" % "0.8.3"
+    )
+  ) dependsOn (client, argonaut)
+
+  lazy val server = Project(
+    "carwings-server",
+    file("server"),
+    settings = Seq(
+      libraryDependencies += "net.databinder" %% "unfiltered-netty-server" % "0.8.3"
+    )
+  ) dependsOn (api, dynamo)
+}
