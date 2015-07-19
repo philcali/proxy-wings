c916fb107b199a56b366136e95b47929a7af15f4
Philip Cali <philip.cali@gmail.com>
2015-07-19 15:09:54 -0400

    executables

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [ -z `which sbt` ] then
+if [ -z `which sbt` ]; then
   curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
   yum install sbt
 fi
