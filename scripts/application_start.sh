4fbab5f1af74299bd1221fe1ba9fde413e53c34d
Philip Cali <philip.cali@gmail.com>
2015-07-19 20:57:16 -0400

    Silence java script execute to complete lifecycle event

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 cd /tmp/carwings
-java -jar server/target/scala-2.11/carwings-server-assembly-1.0.0.jar 80 &
+java -jar server/target/scala-2.11/carwings-server-assembly-1.0.0.jar 80 & > /dev/null 2>&1
