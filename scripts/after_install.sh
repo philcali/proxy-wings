c9990b63e712f4cf3e252f003e6cfb3bf06bc7a6
Philip Cali <philip.cali@gmail.com>
2015-07-19 20:41:59 -0400

    Getting closer

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-echo "$PWD"
+cd /tmp/webapp
 sbt "clean" "project carwings-server" "assembly"
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
+cd /tmp/webapp
 java -jar server/target/scala-2.11/carwings-server-assembly-1.0.0.jar 80 &
