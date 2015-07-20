844b662e333e3945f9c9ac3526a6bdc87340b539
Philip Cali <philip.cali@gmail.com>
2015-07-19 20:31:21 -0400

    Difficule to know which will work

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,6 +3,6 @@
 if [ -e "/usr/bin/sbt" ]; then
   yum -y upgrade sbt
 else
-  curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
+  curl https://bintray.com/sbt/rpm/rpm | tee /etc/yum.repos.d/bintray-sbt-rpm.repo
   yum -y install sbt
 fi
