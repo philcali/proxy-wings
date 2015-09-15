a980f32f61d3f6cff03c13dc9c0992280b8b00be
Philip Cali <philip.cali@gmail.com>
2015-09-14 22:45:18 -0400

    Security complaints

--- server/src/main/scala/Server.scala
+++ server/src/main/scala/Server.scala
@@ -2,12 +2,41 @@ package carwings
 package server
 
 import unfiltered.filter.Planify
-import unfiltered.jetty.Server.{ httpsSysProperties, http }
+import unfiltered.jetty.PropertySslContextProvider
+import unfiltered.jetty.Server.{ http, portBinding, defaultKeystorePathProperty, defaultKeystorePasswordProperty }
+import unfiltered.jetty.SslSocketPortBinding
+import org.eclipse.jetty.util.ssl.SslContextFactory
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.document.DynamoDB
 
 object Server {
+  class SafePropertySslContextProvider extends PropertySslContextProvider(
+    defaultKeystorePathProperty,
+    defaultKeystorePasswordProperty
+  ) {
+    override lazy val sslContextFactory = {
+      val factory = new SslContextFactory
+      factory.setKeyStorePath(keyStorePath)
+      factory.setKeyStorePassword(keyStorePassword)
+      factory.setIncludeCipherSuites(
+        "TLS_DHE_RSA.*",
+        "TLS_ECDHE.*")
+      factory.setExcludeCipherSuites(
+        ".*NULL.*",
+        ".*RC4.*",
+        ".*MD5.*",
+        ".*DES.*",
+        ".*DSS.*")
+      factory.setExcludeProtocols(
+        "SSL",
+        "SSLv2",
+        "SSLv2Hello",
+        "SSLv3")
+      factory
+    }
+  }
+
   def startServer(port: Int, endpoint: Option[String]) {
     val aws = new AmazonDynamoDBClient(new DefaultAWSCredentialsProviderChain())
     endpoint.foreach(aws.setEndpoint)
@@ -16,7 +45,7 @@ object Server {
       val vehicles = dynamodb.VehicleStoreDynamo(db)
     }
     (if (port == 443)
-      httpsSysProperties(port, "0.0.0.0") else
+      portBinding(SslSocketPortBinding(port, "0.0.0.0", new SafePropertySslContextProvider)) else
       http(port, "0.0.0.0")
     ).plan(Planify(proxy.intent))
       .resources(getClass.getClassLoader.getResource("public"))
