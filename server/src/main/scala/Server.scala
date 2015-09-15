e04e2b3c833ac67dff15f290bff745b4929a642a
Philip Cali <philip.cali@gmail.com>
2015-09-14 23:35:37 -0400

    Direct exclusions.

--- server/src/main/scala/Server.scala
+++ server/src/main/scala/Server.scala
@@ -19,17 +19,14 @@ object Server {
       val factory = new SslContextFactory
       factory.setKeyStorePath(keyStorePath)
       factory.setKeyStorePassword(keyStorePassword)
-      /**
-      factory.setIncludeCipherSuites(
-        "TLS_DHE_RSA.*",
-        "TLS_ECDHE.*")
-      */
       factory.setExcludeCipherSuites(
-        ".*NULL.*",
-        ".*RC4.*",
-        ".*MD5.*",
-        ".*DES.*",
-        ".*DSS.*")
+        "SSL_RSA_WITH_DES_CBC_SHA",
+        "SSL_DHE_RSA_WITH_DES_CBC_SHA",
+        "SSL_DHE_DSS_WITH_DES_CBC_SHA",
+        "SSL_RSA_EXPORT_WITH_RC4_40_MD5",
+        "SSL_RSA_EXPORT_WITH_DES40_CBC_SHA",
+        "SSL_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA",
+        "SSL_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA")
       factory.setExcludeProtocols(
         "SSL",
         "SSLv2",
